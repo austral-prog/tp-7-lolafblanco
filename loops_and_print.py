4283f4ef15d9e6c61bb5866281eb4f3da0414df3
lolafblanco <162721872+lolafblanco@users.noreply.github.com>
2024-05-16 23:24:28 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,20 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista: list[str]):
+    newArray = []
 
+    for el in lista:
+        if not el: continue
 
-def enumerate_backwards(list):
-    return list
+        newArray.append(f"{len(newArray)}. {el}")
+
+    return newArray
+
+def enumerate_backwards(lista: list[str]):
+    newArray = []
+
+    for el in lista:
+        if not el: continue
+
+        newArray.append(f"{len(newArray)}. {el[::-1]}")
+
+    return newArray
+
