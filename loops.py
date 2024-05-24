1ca334e4f5c458ecc3988f35788b47b3feaeafa7
lolafblanco <162721872+lolafblanco@users.noreply.github.com>
2024-05-23 22:30:12 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,42 +1,39 @@
-def index_of_by_index(word: str, lista: list[str], index: int):
-    for idx, el in enumerate(lista):
-        if idx < index: continue
-        if el == word: return idx
+def index_of_by_index(x,y,z):
+    index = -1
+    if x in y[z::]:
+        index = y[z::].index(x)
+        index = index+z
+    return index
+index_of_by_index("Magenta",["Red", "Green", "White", "Black", "Pink", "Yellow", "Black", "Green", "Brown", "Red", "Black"], 1)
 
+
+def index_of_empty(colors):
+    for element in colors:
+        if not element:
+            return colors.index(element)
     return -1
+index_of_empty(["Red", "Green", "", "", "Pink", "", "Black"])
 
 
-def index_of_empty(lista: list[str]):
-    for idx, el in enumerate(lista):
-        if el == "": return idx
+def index_of(x,y):
+    index = -1
+    if x in y:
+        index = y.index(x)
+    return index
+index_of("Black",["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"])
 
-    return -1
-
-
-def index_of(word: str, lista: list[str]):
-    for idx, el in enumerate(lista):
-        if el == word: return idx
-
-    return -1
-
-
-def put(word: str, lista: list[str]):
-    for idx, el in enumerate(lista):
-        if el != "": continue
-
-        lista[idx] = word
-        return idx
-
-    return -1
+def put(x, list):
+    if index_of_empty(list) != -1:
+        list[index_of_empty(list)] = x
+        return list.index(x)
+    else:
+        return -1
+put("Blue",["Red", "Green", "", "", "Pink", "", "Black"])
 
 
-def remove(word: str, lista: list[str]):
-    counter = 0
-
-    for idx, el in enumerate(lista):
-        if el != word: continue
-
-        lista[idx] = ""
-        counter += 1
-
-    return counter
+def remove(x, list):
+    a = list.count(x)
+    while x in list:
+        list[list.index(x)] = ""
+    return a
+remove("Blue",["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"])
